54d23b5c82337bc5e6cf6437c3246e2b4dfa4142
Gloria Guy <totallygloria@gmail.com>
2017-01-20 17:53:25 -0800

    Outlines a scoreboard application using Treehouse's tutorial
    This creates the scoreboard component basic template in jsx. The
    player name, current score, and increment and decrement buttons
    are all in place in the rendered DOM, but there's no javascript
    interactions for any of it.

--- app.jsx
+++ app.jsx
@@ -1,10 +1,29 @@
+// class is a reserved word, use className
 
 function Application() {
 
   return(
-    <div>
-      <h1>Hello from React!</h1>
-      <p>This is coming from a React component</p>
+    <div className="scoreboard">
+
+      <div className="header">
+        <h1>Scoreboard</h1>
+      </div>
+
+      <div className="players">
+        <div className="player">
+          <div className="player-name">
+            Gloria Anholt
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score">25</div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
