ddb7bd8b21e26b71aec0d1ec741ab37f2c2149f7
Gloria Guy <totallygloria@gmail.com>
2017-01-24 09:45:04 -0800

    Breaks markup into components for Header, Player, and Counter
    The application was broken into sub React components using the same
    function return style as the Application -- each now takes a default
    "props" argument which refers to the React props object (which you
    can set using the React.propTypes and React.defaultProps built-in
    objects).
    
    The application is now two custom tags - for header and player -
    and the player component contains the custom tag for the counter.
    This breaks the single large app into reusable components.

--- app.jsx
+++ app.jsx
@@ -1,31 +1,70 @@
 // class is a reserved word, use className
 
-function Application() {
+function Header(props) {
+  return (
+    <div className="header">
+        <h1>{props.title}</h1>
+      </div>
+  );
+}
+
+Header.propTypes = {
+  title: React.PropTypes.string.isRequired
+};
+
+function Counter(props) {
+  return (
+    <div className="player-score">
+      <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <div className="counter-score">{props.score}</div>
+        <button className="counter-action increment"> + </button>
+      </div>
+    </div>
+  );
+}
+
+Counter.propTypes = {
+  score: React.PropTypes.number.isRequired
+}
+
+function Player(props) {
+  return (
+    <div className="player">
+      <div className="player-name">
+        {props.name}
+      </div>
+      <Counter score={props.score} />
+    </div>
+  );
+}
+
+Player.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  score: React.PropTypes.number.isRequired
+}
+
+function Application(props) {
 
   return(
     <div className="scoreboard">
-
-      <div className="header">
-        <h1>Scoreboard</h1>
-      </div>
+      <Header title={props.title}/>
 
       <div className="players">
-        <div className="player">
-          <div className="player-name">
-            Gloria Anholt
-          </div>
-          <div className="player-score">
-            <div className="counter">
-              <button className="counter-action decrement"> - </button>
-              <div className="counter-score">25</div>
-              <button className="counter-action increment"> + </button>
-            </div>
-          </div>
-        </div>
+        <Player name="Gloria Anholt" />
+        <Player name="Erica Hendricks" />
       </div>
 
     </div>
   );
 }
 
-ReactDOM.render(<Application />, document.getElementById('container'));
+Application.propTypes = {
+  title: React.PropTypes.string  // .isRequired as an optional last arg
+};
+
+Application.defaultProps = {
+  title: "Scoreboard"
+};
+
+ReactDOM.render(<Application title="React Scoreboard"/>, document.getElementById('container'))
