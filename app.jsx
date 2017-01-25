a71be5e588bfcec365eca7b734c4a8dc5ad15c04
Gloria Guy <totallygloria@gmail.com>
2017-01-25 09:21:01 -0800

    Moves players into a player array, creates components from array
    JSX can't have expressions in it, only statements, so a for loop
    won't work. Instead, you can use a map array method to iterate over
    the player array and return components using the data.
    
    Iteration in React requires having a unique "key" attribute on your
    data so it can more efficiently re-render only what is necessary
    when changes are made to the data.

--- app.jsx
+++ app.jsx
@@ -1,5 +1,11 @@
 // class is a reserved word, use className
 
+var Players = [
+  { name: "Gloria Anholt", score: 25, id: 1 },
+  { name: "Erica Hendricks", score: 25, id: 2 },
+  { name: "Eric Anholt", score: 26, id: 3 },
+]
+
 function Header(props) {
   return (
     <div className="header">
@@ -51,8 +57,9 @@ function Application(props) {
       <Header title={props.title}/>
 
       <div className="players">
-        <Player name="Gloria Anholt" />
-        <Player name="Erica Hendricks" />
+        {props.players.map(function(player) {
+          return <Player name={player.name} score={player.score} key={player.id} />
+        })}
       </div>
 
     </div>
@@ -60,11 +67,16 @@ function Application(props) {
 }
 
 Application.propTypes = {
-  title: React.PropTypes.string  // .isRequired as an optional last arg
+  title: React.PropTypes.string,  // .isRequired as an optional last arg
+  players: React.PropTypes.arrayOf(React.PropTypes.shape({
+    name: React.PropTypes.string.isRequired,
+    score: React.PropTypes.number.isRequired,
+    id: React.PropTypes.number.isRequired,
+  })).isRequired
 };
 
 Application.defaultProps = {
   title: "Scoreboard"
 };
 
-ReactDOM.render(<Application title="React Scoreboard"/>, document.getElementById('container'))
+ReactDOM.render(<Application players={Players} />, document.getElementById('container'))
